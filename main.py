266fa0d8b4b55f34f5e5fed5f3f3536b64914bc8
hemalthakkar <hemalthakkar2201gmail.com>
2025-06-05 18:34:39 -0400

    Add file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-echo print("Hello from Lab2!") > main.py
+echo print("Hello from Lab2!") 
 
 # Create a proper main.py file
-echo print("Hello from Lab2 - Hemal!") > main.py
-echo print("This is my first Python project with Git") >> main.py
-echo print("Lab 2 - Version Control and CI/CD") >> main.py
+echo print("Hello from Lab2 - Hemal!") 
+echo print("This is my first Python project with Git") 
+echo print("Lab 2 - Version Control and CI/CD") 
